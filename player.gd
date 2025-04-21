f41700db8bd75bb419f1cba87fa95749e580da59
newcovid <280310454@qq.com>
2025-04-21 17:32:27 +0800

    Initial commit of Godot project

---
+++ player.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+@export var speed = 400
+var  screen_size
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	#hide()
+	
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D_1.play()
+	else:
+		$AnimatedSprite2D_1.stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+	if velocity.x != 0:
+		$AnimatedSprite2D_1.animation = "walk"
+		$AnimatedSprite2D_1.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D_1.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D_1.animation = "up"
+		$AnimatedSprite2D_1.flip_v = velocity.y > 0
